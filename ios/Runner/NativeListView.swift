ae21b1bcc3e1a7002407b2fdb54be09f90f34b4d
Moonpil Cheon <iampeel@naver.com>
2025-04-04 12:11:16 +0900

    native connection. 안드로이드는 아직.

---
+++ ios/Runner/NativeListView.swift
@@ -0,0 +1,8 @@
+//
+//  NativeListView.swift
+//  Runner
+//
+//  Created by 천문필 on 4/3/25.
+//
+
+import Foundation
---
+++ ios/Runner/NativeListViewFactory.swift
@@ -0,0 +1,8 @@
+//
+//  NativeListViewFactory.swift
+//  Runner
+//
+//  Created by 천문필 on 4/3/25.
+//
+
+import Foundation
